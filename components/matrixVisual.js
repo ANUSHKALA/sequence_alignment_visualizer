7e18ceecebd8e9c8c31bf19fea2f9e43e46e9e13
Anshuman Swain <anshuman8swain@gmail.com>
2023-07-01 14:22:21 +0530

    test:needleman

--- components/matrixVisual.js
+++ components/matrixVisual.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 
-const MatrixVisual = ({ matrix }) => {
+const MatrixVisual = ({ matrix}) => {
   const canvasRef = useRef(null);
-  const cellSize = 30; // Adjust this value based on your preference
+  const cellSize = 50; // Adjust this value based on your preference
 
   useEffect(() => {
+    console.log("Matrix visual for: ", matrix);
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-
+    canvas.height=matrix.length*cellSize;
+    canvas.width=matrix[0].length*cellSize;
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -31,31 +33,28 @@ const MatrixVisual = ({ matrix }) => {
       }
     }
 
-    // Draw arrows (example for Needleman-Wunsch traceback)
-    for (let i = 1; i < matrix.length; i++) {
-      for (let j = 1; j < matrix[i].length; j++) {
-        const cellX = j * cellSize;
-        const cellY = i * cellSize;
-        const score = matrix[i][j];
-        const maxNeighborScore = Math.max(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i][j - 1]);
+    // // Draw arrows (example for Needleman-Wunsch traceback)
+    // for (let i = 1; i < arrowMatrix.length; i++) {
+    //   for (let j = 1; j < arrowMatrix[i].length; j++) {
+    //     if (arrowMatrix[i][j]) {
+    //       const cellX = j * cellSize;
+    //       const cellY = i * cellSize;
 
-        if (score === maxNeighborScore) {
-          // Draw arrow from current cell to highest-scoring neighbor
-          ctx.beginPath();
-          ctx.moveTo(cellX + cellSize / 2, cellY + cellSize / 2);
-          ctx.lineTo(cellX + cellSize / 2, cellY - cellSize / 2);
-          ctx.lineTo(cellX - cellSize / 2, cellY + cellSize / 2);
-          ctx.closePath();
-          ctx.fillStyle = 'black';
-          ctx.fill();
-        }
-      }
-    }
+    //       // Draw arrow from current cell to highest-scoring neighbor
+    //       ctx.beginPath();
+    //       ctx.moveTo(cellX + cellSize / 6, cellY + cellSize / 6);
+    //       ctx.lineTo(cellX + cellSize / 6, cellY - cellSize / 6);
+    //       ctx.lineTo(cellX - cellSize / 6, cellY + cellSize / 6);
+    //       ctx.closePath();
+    //       ctx.fillStyle = 'black';
+    //       ctx.fill();
+    //     }
+    //   }
+    // }
   }, [matrix]);
 
   const getColorFromScore = (score) => {
-    // Define your color scale based on the score range
-    // Example: Higher scores are warmer colors, lower scores are cooler colors
+
     const minScore = -10;
     const maxScore = 10;
     const minHue = 0; // Adjust hue values as per your color scheme
