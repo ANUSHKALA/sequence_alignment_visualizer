58bb9b4320b30af98895392c6fdc859b7aaea2c8
ANUSHKALA <anushkakaimal2003@gmail.com>
2023-07-19 01:57:45 +0530

    ui fixes

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,26 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={""}>
-      <div className=" h-screen  bg-gray-900 flex items-center justify-center">
+      <div className=" h-screen bg-gradient-to-r from-rose-200 via-orange-100 to-amber-200 flex items-center justify-center">
         <div className=" flex flex-wrap my-20 ">
           <div className="text-justify">
             <div className="text-center text-2xl capitalize px-auto p-10 pb-20">
-              <h2 className="md:text-5xl text-4xl font-bold text-gray-300 ">
+              <h2 className="md:text-5xl text-4xl font-bold text-gray-900 ">
                 Sequence Alignment Visualization
               </h2>
             </div>
             <div className="grid grid-rows-1 ">
               <Link
-                className="mx-auto text-center p-4 bg-gray-300 mb-6 md:w-96 p-10 w-64 "
+                className="mx-auto text-center mb-6 md:w-96 px-4 py-4 w-64  font-bold border-b-4 border-gray-700 hover:border-gray-900 "
                 href="/global"
               >
-                <h3 className="text-2xl text-gray-700">Global Algorithm</h3>
+                <h3 className="text-2xl hover:text-3xl text-gray-700">Global Alignment</h3>
               </Link>
               <Link
-                className="mx-auto text-center p-4 bg-gray-300 md:w-96 p-10 w-64"
+                className="mx-auto text-center mb-6 md:w-96 px-4 py-4 w-64 font-bold border-b-4 border-gray-700 hover:border-gray-900 "
                 href="/local"
               >
-                <h3 className="text-2xl text-gray-700">Local Algorithm</h3>
+                <h3 className="text-2xl hover:text-3xl text-gray-700">Local Alignment</h3>
               </Link>
             </div>
           </div>
